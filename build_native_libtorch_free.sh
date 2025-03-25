87a325a1ce27fbb98f71c6bdb4d563cc07161b6b
Bin Bao <binbao@meta.com>
2025-03-24 17:31:46 -0700

    Split the build and run script

---
+++ build_native_libtorch_free.sh
@@ -0,0 +1,4 @@
+#AOT_INDUCTOR_DEBUG_COMPILE=1
+#AOT_INDUCTOR_LIBTORCH_FREE=1 python3 torchchat.py export llama3.1 --output-aoti-package-path exportedModels/llama3_1_artifacts_libtorch_free.pt2
+rm -rf cmake-out
+torchchat/utils/scripts/build_native.sh aoti_libtorch_free
--- run_native.sh
+++ run_native.sh
@@ -1,3 +1,1 @@
-#LD_LIBRARY_PATH=/home/binbao/local/miniconda3/envs/pytorch-3.11/lib/ cmake-out/aoti_run exportedModels/llama3_1_artifacts.pt2 -z `python3 torchchat.py where llama3.1`/tokenizer.model -i "Once upon a time"
-
-cmake-out/aoti_libtorch_free_run exportedModels/llama3_1_artifacts_libtorch_free.pt2 -z `python3 torchchat.py where llama3.1`/tokenizer.model -i "Once upon a time"
+LD_LIBRARY_PATH=/home/binbao/local/miniconda3/envs/pytorch-3.10/lib/ cmake-out/aoti_run exportedModels/llama3_1_artifacts.pt2 -z `python3 torchchat.py where llama3.1`/tokenizer.model -i "Once upon a time"
